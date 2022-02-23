4e9bee0a26e03e5febdafce48846ad743d4d3641
Haibo Fang (fangh13) <fangh13@coventry.ac.uk>
2022-02-23 23:03:21 +0000

    添加项目文件。

---
+++ ToDoAPP/ToDoAPP/Module/MenuModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ToDoApp.Module
+{
+    
+    //单项
+    public class MenuModel : ViewModelBase
+    {
+        public string IconFont { get; set; }
+
+        public string Title { get; set; }
+
+        public string BackColor { get; set; }
+
+
+        private ObservableCollection<TaskInfo> taskInfos = new ObservableCollection<TaskInfo>();
+
+        public ObservableCollection<TaskInfo> TaskInfos
+        {
+            get { return taskInfos; }
+            set { taskInfos = value; RaisePropertyChanged(); }
+        }
+    }
+
+    public class TaskInfo 
+    {
+        public string Content { get; set; }
+
+    }
+}
---
+++ ToDoAPP/ToDoAPP/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using ToDoApp.Module;
+
+namespace ToDoApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            MenuModels = new ObservableCollection<MenuModel>();
+            menuModels.Add(new MenuModel() { IconFont = "\xe635", Title = "My day", BackColor = "#218868", });
+            menuModels.Add(new MenuModel() { IconFont = "\xe6b6", Title = "Important", BackColor = "#EE3B3B", });
+            menuModels.Add(new MenuModel() { IconFont = "\xe6e1", Title = "Planned", BackColor = "#218868", });
+            menuModels.Add(new MenuModel() { IconFont = "\xe614", Title = "Assigned Task", BackColor = "#EE3B3B", });
+            menuModels.Add(new MenuModel() { IconFont = "\xe755", Title = "Task", BackColor = "#218868", });
+        }
+        private ObservableCollection<MenuModel> menuModels;
+
+        public ObservableCollection<MenuModel> MenuModels
+        {
+            get { return menuModels; }
+            set { menuModels = value; RaisePropertyChanged(); }
+        }
+
+    }
+}
