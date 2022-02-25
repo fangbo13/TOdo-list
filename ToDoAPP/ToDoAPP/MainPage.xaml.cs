464a26ecbc8d41d55809a715ecc1bbe348f039cc
Haibo Fang (fangh13) <fangh13@coventry.ac.uk>
2022-02-25 23:25:02 +0000

    Finish the static page, add the converter, and create the first branch

--- ToDoAPP/ToDoAPP/MainPage.xaml.cs
+++ ToDoAPP/ToDoAPP/MainPage.xaml.cs
@@ -19,5 +19,11 @@ namespace ToDoAPP
             InitializeComponent();
             this.BindingContext =new MainViewModel();/*绑定*/
         }
+
+        //private void ListView_ItemSelected(object sender,SelectedPositionChangedEventArgs e)
+        //{
+        //    ListView lv = sender as ListView;
+        //    lv.SelectedItem = null;/*背景颜色消失*/
+        //}
     }
 }
