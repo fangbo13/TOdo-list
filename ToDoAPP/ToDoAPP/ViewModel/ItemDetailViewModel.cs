537761d773ff8f05f878402a2ac21a3891636bf5
Haibo Fang (fangh13) <fangh13@coventry.ac.uk>
2022-02-27 05:17:09 +0000

    Edit content page, adaptive background colour, add test task list

---
+++ ToDoAPP/ToDoAPP/ViewModel/ItemDetailViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using ToDoApp.Module;
+
+//后台业务代码
+namespace ToDoAPP.ViewModel
+{
+    class ItemDetailViewModel : ViewModelBase
+    {
+        public ItemDetailViewModel(ObservableCollection<TaskInfo> TaskInfos)
+        {
+            this.TaskInfos = TaskInfos;//绑定值
+
+            TaskInfos.Add(new TaskInfo() { });//添加测试内容
+            TaskInfos.Add(new TaskInfo() { });
+            TaskInfos.Add(new TaskInfo() { });
+        }
+
+        private ObservableCollection<TaskInfo> taskInfos = new ObservableCollection<TaskInfo>();
+
+        public ObservableCollection<TaskInfo> TaskInfos
+        {
+            get { return taskInfos; }
+            set { taskInfos = value; RaisePropertyChanged(); }
+        }
+    }
+}
+
