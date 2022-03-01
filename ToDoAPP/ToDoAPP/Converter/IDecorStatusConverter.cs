bec93c234d48cec4b340e3296cf05db91359f8b5
Haibo Fang (fangh13) <fangh13@coventry.ac.uk>
2022-03-01 23:32:21 +0000

    Binding selection and underlining functions

---
+++ ToDoAPP/ToDoAPP/Converter/IDecorStatusConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ToDoApp.Converter
+{
+    public class IDecorStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && bool.TryParse(value.ToString(), out bool result))/*判断结果不等于null并且是布尔类型 返回下滑线结果*/
+            {
+                if (result) return TextDecorations.Strikethrough;/*返回划线效果*/
+            }
+            return TextDecorations.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return TextDecorations.None;
+        }
+    }
+}
