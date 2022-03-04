0294e120b5c6e552df8e620b708515693cd962dc
Haibo Fang (fangh13) <fangh13@coventry.ac.uk>
2022-03-04 09:01:17 +0000

    Injection into Autofac

--- ToDoAPP/ToDoAPP/App.xaml.cs
+++ ToDoAPP/ToDoAPP/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ToDoApp.Core;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,6 +11,9 @@ namespace ToDoAPP
         {
             InitializeComponent();
 
+            AutofacLocator autofac = new AutofacLocator();
+            autofac.Register();
+            ServiceProvider.RegisterSerivceLocator(autofac);
             MainPage = new NavigationPage(new MainPage());/*把mainpage当为导航页，当点击功能时通过navigation.pushAsync（）推送的屏幕上方*/
         }
 
---
+++ ToDoAPP/ToDoAPP/Core/AutofacLocator.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Core
+{
+    public class AutofacLocator : IAutofacLocator
+    {
+        IContainer container;
+
+        public TInterface Get<TInterface>()
+        {
+            return container.Resolve<TInterface>();
+        }
+
+        /// <summary>
+        /// 注册容器映射关系
+        /// </summary>
+        public void Register()
+        {
+            var Container = new ContainerBuilder();
+
+            //注入ToDo数据层服务映射
+            //Container.RegisterType<ToDoService>().As<IToDoService>();
+
+            container = Container.Build();
+        }
+    }
+
+    //全局容器管理器
+    public class ServiceProvider
+    {
+        public static IAutofacLocator Instance { get; private set; }
+
+        public static void RegisterSerivceLocator(IAutofacLocator locator)
+        {
+            Instance = locator;
+        }
+    }
+}
