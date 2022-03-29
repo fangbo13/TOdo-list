f731a2c7d184633506b035bf5ea79f0fe6923457
Haibo Fang(fangh13) <>
2022-03-30 01:55:38 +0800

    Add basics for http api

---
+++ ToDoAPP/ToDoAPP/Core/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoAPP.Core
+{
+    public class Result<T> where T : class
+    {
+        /// <summary>
+        /// 结果代码
+        /// </summary>
+        public string ResultCode { get; set; }
+
+        /// <summary>
+        /// 结果消息
+        /// </summary>
+        public string ResultMsg { get; set; }
+
+        /// <summary>
+        /// 返回结果
+        /// </summary>
+        public T ResultData { get; set; }
+
+        public bool IsSuccess => ResultCode?.Trim() == "200";
+    }
+}
---
+++ ToDoAPP/ToDoAPP/Service/APIService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoAPP.Core;
+using ToDoAPP.Core.Helper;
+
+namespace ToDoAPP.Service
+{
+    public class APIService
+    {
+        private const string GET_FTPINFO_URL = "/api/jkcEquipment/GetFtpInfo";
+     
+        public Result<string> GetFtpInfo()
+        {
+            try
+            {
+                var Api_result = HttpHelper.Instance.Request<Result<string>>(GET_FTPINFO_URL, System.Net.Http.HttpMethod.Post);
+                List<JObject> result = JsonConvert.DeserializeObject<List<JObject>>(Api_result.ResultData);
+                foreach (var item in result)
+                {
+                    //MapInfo m = new MapInfo { Name = item.SHIFTNAME, Value = item.SHIFTNAME };
+                    //MInfo.Add(m);
+                }
+
+                if (result == null)
+                    return new Result<string>();
+                return Api_result;
+            }
+            catch (Exception e)
+            {
+                Result<string> result = new Result<string>();
+                result.ResultMsg= e.Message ;
+                result.ResultCode = "201";
+                return result;
+            }
+        }
+    }
+}
